5b127c7584f38dd1e35c7e766759f529585cd8e0
atherson <athersonfrank5@gmail.com>
2026-03-20 17:42:23 +0300

    auto: 2026-03-20 17:42:23

---
+++ autosync.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+WATCH="$HOME/.config/hypr"
+TARGET="$HOME/hyprdots/Configs/.config/hypr"
+REPO="$HOME/hyprdots"
+
+while true; do 
+	inotifywait -r -e modify,create,delete,move "$WATCH" 
+	
+	rsync -a --delete "$WATCH/" "$TARGET/"
+	
+	cd "$REPO" || exit
+	
+	git add .
+
+	if ! git diff --cached --quiet; then git commit -m "auto: $(date '+%Y-%m-%d %H:%M:%S')"
+		git push
+	fi
+done
